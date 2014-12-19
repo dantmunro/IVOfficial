e88e2c93311e3b75e67d621a1a01c9b50ea635e5
d.munro <d.munro@wustl.edu>
2014-12-18 18:12:48 -0600

    Added all initial files

---
+++ main.lua
@@ -0,0 +1,9 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+local splash = display.newGroup()
+local background = display.newImage( "splashscreen.png", display.contentCenterX, display.contentCenterY )
